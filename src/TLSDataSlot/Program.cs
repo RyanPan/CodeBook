68c7486c75eedabaa4b6cf72bbf9c162ff946021
Ryan Pan <ryan.ppan@gmail.com>
2016-11-24 13:53:37 +0800

    练习：从零开始学.net多线程系列(一)（http://blog.csdn.net/yanghua_kobe/article/details/6961216）

---
+++ src/TLSDataSlot/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TLSDataSlot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread[] threadArray = new Thread[4];
+
+            for (int i = 0; i < threadArray.Length; i++)
+            {
+                threadArray[i] = new Thread(new ThreadStart(Slot.SlotTest));
+                threadArray[i].Start();
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class Slot
+    {
+        static Random randomGenerator = new Random();
+
+        public static void SlotTest()
+        {
+            // Set different data in each thread's data slot.
+            Thread.SetData(Thread.GetNamedDataSlot("Random"), randomGenerator.Next(1, 20));
+
+            // Write the data from each thread's data slot.
+            Console.WriteLine("Data in thread_{0}'s data slot: {1,3}", AppDomain.GetCurrentThreadId(), Thread.GetData(Thread.GetNamedDataSlot("Random")));
+
+            // Allow other thread time to execute SetData to show that a thread's data slot is unique to the thread.
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Data in thread_{0}'s data slot it still: {1,3}", AppDomain.GetCurrentThreadId(), Thread.GetData(Thread.GetNamedDataSlot("Random")));
+
+            // Allow time for other threads to show their data, then demonstrate that any code a thread executes has access to the thread's named data slot.
+            Thread.Sleep(1000);
+
+            Other o = new Other();
+            o.ShowSlotData();
+        }
+    }
+
+    class Other
+    {
+        public void ShowSlotData()
+        {
+            // This method has no access to access the data in the Slot class, but when executed by a thread it can obtain the thread's data from a named slot.
+            Console.WriteLine("Other data displays data in thread_{0}'s data slot: {1,3}", AppDomain.GetCurrentThreadId(), Thread.GetData(Thread.GetNamedDataSlot("Random")));
+        }
+    }
+}
---
+++ src/ThreadStaticAttributeTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadStaticAttributeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Thread thread = new Thread(new ThreadStart(ThreadData.ThreadStaticDemo));
+                thread.Start();
+            }
+        }
+    }
+
+    class ThreadData
+    {
+        [ThreadStatic]
+        static int threadSpecificData;
+
+        public static void ThreadStaticDemo()
+        {
+            // Store the managed thread id for each thread in the static variable.
+            threadSpecificData = Thread.CurrentThread.ManagedThreadId;
+
+            // Allow other thread time to execute the same code, to show that the static data is unique to each thread.
+            Thread.Sleep(1000);
+
+            // Display the static data.
+            Console.WriteLine("Data for managed thread {0}: {1}", Thread.CurrentThread.ManagedThreadId, threadSpecificData);
+        }
+    }
+}
