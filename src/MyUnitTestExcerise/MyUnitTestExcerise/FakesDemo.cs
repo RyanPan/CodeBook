094d422257b56b3e3b18df3e9b31f93a45bd265e
Panzhenhua <panzhenhua@didilife.com>
2017-03-10 13:47:09 +0800

    xUnit demo

---
+++ src/MyUnitTestExcerise/MyUnitTestExcerise/FakesDemo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUnitTestExcerise.FakesDemo
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+
+    public interface ICustomerRepository
+    {
+        Customer GetById(int Id);
+        IEnumerable<Customer> GetAll();
+        Customer SaveOrUpdate(Customer customer);
+        void Delete(Customer customer);
+    }
+
+    public class CustomerRepository : ICustomerRepository
+    {
+        public void Delete(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer SaveOrUpdate(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ViewModelBase
+    {
+
+    }
+
+    public class CustomerViewModel : ViewModelBase
+    {
+        private Customer customer;
+        private readonly ICustomerRepository repository;
+
+        public CustomerViewModel(Customer customer, ICustomerRepository repository)
+        {
+            this.customer = customer;
+            this.repository = repository;
+        }
+
+        public string Name
+        {
+            get { return this.customer.Name; }
+            set
+            {
+                customer.Name = value;
+                // RaisePropertyChanged("Name");
+            }
+        }
+
+        public void Save()
+        {
+            customer = repository.SaveOrUpdate(customer);
+        }
+    }
+}
---
+++ src/MyUnitTestExcerise/MyUnitTestExceriseTestsUsingxUnit/MyFirstUnitTests.cs
@@ -0,0 +1,35 @@
+﻿using MyUnitTestExcerise.FakesDemo;
+using MyUnitTestExcerise.FakesDemo.Fakes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyUnitTestExceriseTestsUsingxUnit
+{
+    public class MyFirstUnitTests
+    {
+        [Fact]
+        public void SaveShouldUpdateTheRepository()
+        {
+            // Arrange
+            var savedCustomer = default(Customer);
+            var repository = new StubICustomerRepository
+            {
+                SaveOrUpdateCustomer = customer => savedCustomer = customer
+            };
+            var actualCustomer = new Customer { Id = 1, Name = "Peter", Email = "peter@costoso.com" };
+            var viewModel = new CustomerViewModel(actualCustomer, repository);
+
+            // Act
+            viewModel.Save();
+
+            // Assert
+            Assert.NotNull(savedCustomer);
+            Assert.Equal(1, savedCustomer.Id);
+            Assert.Equal("Peter", savedCustomer.Name);
+        }
+    }
+}
